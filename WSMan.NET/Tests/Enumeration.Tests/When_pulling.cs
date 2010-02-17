0d6f52006c96e006098dfde8365102d8c15ae0f0
SzymonPobiega_cp <7e1df2fc7b7eb1de3ab65aeecb7227e75e7ca2bfAUJZqsgx>
2010-02-17 16:56:52 +0000

    Fixed bug connected with Java web service returning xsi:type in EndOfSequence element.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS02]$/wsmannet;C39900

--- WSMan.NET/Tests/Enumeration.Tests/When_pulling.cs
+++ WSMan.NET/Tests/Enumeration.Tests/When_pulling.cs
@@ -1,15 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
 using System.Text;
+using System.Xml.Serialization;
 using NUnit.Framework;
 
 namespace WSMan.NET.Enumeration.Tests
 {
    [TestFixture]
    public class When_pulling
-   {      
+   {
+      [Test]
+      public void Additional_data_in_response_is_ignored()
+      {
+         const string xml = @"<ns5:PullResponse xmlns:ns3=""http://schemas.xmlsoap.org/ws/2004/08/addressing""
+    xmlns:ns4=""http://schemas.xmlsoap.org/ws/2004/08/eventing""
+    xmlns:ns5=""http://schemas.xmlsoap.org/ws/2004/09/enumeration""
+    xmlns:ns6=""http://schemas.xmlsoap.org/ws/2004/09/transfer""
+    xmlns:ns7=""http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd""
+    xmlns:ns8=""http://schemas.dmtf.org/wbem/wsman/identity/1/wsmanidentity.xsd""
+    xmlns:ns9=""http://jsr262.dev.java.net/jmxconnector"">
+      <ns5:Items>
+        <ns3:EndpointReference>
+          <ns3:Address>http://iblongsw573721:10003/</ns3:Address>
+          <ns3:ReferenceParameters>
+            <ns7:ResourceURI>
+            http://jsr262.dev.java.net/DynamicMBeanResource</ns7:ResourceURI>
+            <ns7:SelectorSet>
+              <ns7:Selector Name=""ObjectName"">
+              JMImplementation:type=MBeanServerDelegate</ns7:Selector>
+            </ns7:SelectorSet>
+          </ns3:ReferenceParameters>
+        </ns3:EndpointReference>
+      </ns5:Items>
+      <ns5:EndOfSequence xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xs=""http://www.w3.org/2001/XMLSchema"" xsi:type=""xs:string""></ns5:EndOfSequence>
+    </ns5:PullResponse>";
+
+         AddressingVersionExtension.Activate(AddressingVersion.WSAddressingAugust2004);
+         EnumerationModeExtension.Activate(EnumerationMode.EnumerateEPR, null);
+         XmlSerializer xs = new XmlSerializer(typeof(PullResponse), null, new Type[]{typeof(string)}, new XmlRootAttribute("PullResponse") { Namespace = "http://schemas.xmlsoap.org/ws/2004/09/enumeration" }, null);         
+         object deserializedObject = xs.Deserialize(new StringReader(xml));
+      }
+
       [Test]
       [ExpectedException(typeof(FaultException))]
       public void If_invalid_subscription_context_specified_exception_is_thrown()
--- WSMan.NET/WSMan.NET/Enumeration/EndOfSequence.cs
+++ WSMan.NET/WSMan.NET/Enumeration/EndOfSequence.cs
@@ -1,8 +1,10 @@
 using System.Linq;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace WSMan.NET.Enumeration
 {
+   [XmlType("string", Namespace="http://www.w3.org/2001/XMLSchema")]
    public class EndOfSequence
    {
    }
